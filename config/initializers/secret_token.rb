99184a6af7adbc60593a6f0897257720731d7c15
purplehaze <purplehaze@debian.vegangravity.com>
2014-04-13 16:50:03 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Foremtest::Application.config.secret_key_base = 'ca9ee2801330f44091bac0aa9ce2a1276cb6c1046a6156135601a2c3cd8fe465782b82d4115ca5f8e4dc31d320e7930db9b4834da59a46b7e35db3094f2a9265'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Foremtest::Application.config.session_store :cookie_store, key: '_foremtest_session'
